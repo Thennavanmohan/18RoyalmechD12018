75b7bee17d4d1ac84f8f70dffd1293d964233ab9
Thennavanmohan <42933326+Thennavanmohan@users.noreply.github.com>
2018-09-24 14:51:02 +0530

    Add files via upload

---
+++ mark.py
@@ -0,0 +1,14 @@
+a=int(input('enter the marks:'))
+PRINT('your grade is:')
+if(a>=90):
+	print('A+')
+elif(a>=80):
+	print('A')
+elif(a>=70):
+	print('B+')
+elif(a>=60):
+	print('B')
+elif(a>=50):
+	print('c')
+else:
+	print('fail!')
