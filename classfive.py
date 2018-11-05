f2906bcc60e05115e12366224890177b826d1d50
Thennavanmohan <42933326+Thennavanmohan@users.noreply.github.com>
2018-11-05 16:22:05 +0530

    Add files via upload

---
+++ classfive.py
@@ -0,0 +1,14 @@
+
+class Badboys:
+	x=[]
+	y=[]
+	for i in range(0,2):
+		name=input('type the name..!')
+		usn=input('your USN:')
+		x.append(name)
+		y.append(usn)
+	for j in range(0,2):
+		print('Name:',x[i])
+		print('age:',y[j])
+p=Badboys()				
+
